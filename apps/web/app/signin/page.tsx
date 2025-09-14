d10062e7707dcf8064b7f5e15c39023beed4deb7
nandeeshlaxetti-prog <nandeeshlaxetti@gmail.com>
2025-09-14 17:23:09 +0530

    Fix syntax error in signin page
    - Remove problematic SVG background pattern with conflicting quotes
    - Replace with simpler background pattern
    - Resolves JSX compilation error

--- apps/web/app/signin/page.tsx
+++ apps/web/app/signin/page.tsx
@@ -32,7 +32,9 @@ export default function SignInPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       {/* Background Pattern */}
-      <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23f3f4f6" fill-opacity="0.1"%3E%3Ccircle cx="30" cy="30" r="1"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-40"></div>
+      <div className="absolute inset-0 opacity-40">
+        <div className="absolute inset-0 bg-gray-100 bg-opacity-20"></div>
+      </div>
       
       <div className="relative sm:mx-auto sm:w-full sm:max-w-md">
         {/* Logo/Brand */}
